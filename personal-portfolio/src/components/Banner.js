ef9cd07031d9f36b75155acb32c2376be93e9c53
Manjari Prasad <manjari.prasad19@gmail.com>
2025-02-10 02:52:20 -0800

    created banner.js. add the about me information later

---
+++ personal-portfolio/src/components/Banner.js
@@ -0,0 +1,21 @@
+import { Container, Row } from "react-bootstrap"
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio!</span>
+                        <h1>{`Hi I'm webdecoded`}<span className="wrap">web developer</span></h1>
+                        <p>
+                            ADD ABOUT ME LATER! FRAME IT PROPERLY
+                        </p>
+                        <button onClick={()=>console.log('connect')}>Let's connect</button>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    )
+}
